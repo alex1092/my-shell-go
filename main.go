729e4099e408e15d0c59fbef10ae5be569aafa71
Alex Little <alex.little@neds.com>
2024-06-27 23:03:33 +0800

    adds gitingnore

--- main.go
+++ main.go
@@ -1,44 +1,93 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"github.com/c-bata/go-prompt"
 )
 
 func main() {
-	for {
-		fmt.Fprint(os.Stdout, "$ ")
-		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionPrefix("$ "),
+	)
+
+	p.Run()
+}
+
+func executor(in string) {
+	clearIn := strings.TrimSpace(in)
+	cmds := strings.Split(clearIn, " ")
+
+	switch cmds[0] {
+	case "exit":
+		os.Exit(0)
+	case "pwd":
+		dir, err := os.Getwd()
 		if err != nil {
 			fmt.Println(err.Error())
-			continue
+		} else {
+			fmt.Println(dir)
 		}
-		clearIn := strings.TrimSpace(in)
-		cmds := strings.Split(clearIn, " ")
+	case "cd":
+		cd(cmds[1:])
+	case "nvim":
+		nvim(cmds[1:])
+	case "echo":
+		fmt.Println(strings.Join(cmds[1:], " "))
+	case "type":
+		handleType(cmds[1:])
+	case "cursor":
+		cursor(cmds[1:])
+	default:
+		runCommand(cmds)
+	}
+}
 
-		switch cmds[0] {
-		case "exit":
-			os.Exit(0)
-		case "pwd":
-			dir, err := os.Getwd()
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				fmt.Println(dir)
-			}
-		case "cd":
-			cd(cmds[1:])
-		case "echo":
-			fmt.Println(strings.Join(cmds[1:], " "))
-		case "type":
-			handleType(cmds[1:])
-		default:
-			runCommand(cmds)
-		}
+func completer(d prompt.Document) []prompt.Suggest {
+	word := d.GetWordBeforeCursor()
+	if word == "" {
+		return nil
+	}
+	s := []prompt.Suggest{
+		{Text: "exit", Description: "Exit the shell"},
+		{Text: "pwd", Description: "Print working directory"},
+		{Text: "cd", Description: "Change directory"},
+		{Text: "nvim", Description: "Open file with nvim"},
+		{Text: "echo", Description: "Echo arguments"},
+		{Text: "type", Description: "Describe a command"},
+		{Text: "cursor", Description: "Run cursor command"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func cursor(args []string) {
+	cmd := exec.Command("cursor", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func nvim(args []string) {
+	if len(args) == 0 {
+		fmt.Println("code: missing file operand")
+		return
+	}
+	cmd := exec.Command("nvim", args[0])
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("code: %s\n", err.Error())
 	}
 }
 
